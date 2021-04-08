e6e9e2cba3db3d1b2f5e27c2ad21e08cc4ace008
clearclarencs <jamesskipworth007@gmail.com>
2021-04-08 22:00:10 +0100

    Alpha

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ settings.json needs bot name, the dashboard cookie for the quicktask as describe
 
 For cybersole cookie is dashboard_session and should be formatted "dashboard_session=COOKIE;"
 
-For channels and keywords.txt each line is an or and for keywords a , represends and, an example of these are already in the files.
+For channels, domains and keywords.txt each line is an or and for keywords a , represends and, an example of these are already in the files.
+
+domains.txt is just a link keyword so any part of a domain can be used
 
 This is a mimic of NikeShoeBots "blue task" and is not created with the involvement of any of these companies.
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 # Discord qt monitor
-import discord, json, requests
+import discord, json, requests, time
 from discord.ext import commands
 
 client=commands.Bot(command_prefix="/")
 
 keywords = []
 channels = []
+domains = []
 with open("settings.json", "r") as r:
     settings = json.loads(r.read())
 
 def refresh():
-    global keywords, channels
+    global keywords, channels, domains
     with open("channels.txt", "r") as r:
         channels = r.read().splitlines()
     with open("keywords.txt", "r") as r:
         keywords = r.read().splitlines()
+    with open("domains.txt", "r") as r:
+        domains = r.read().splitlines()
+
 
 
 def go(ctx):
     if settings["bot"] == "cybersole":
         headers = {"Cookie":settings["botCookie"], "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"}
-        requests.get("https://cybersole.io/dashboard/tasks?quicktask="+ctx.embeds[0].url, headers=headers)
+        #requests.get("https://cybersole.io/dashboard/tasks?quicktask="+ctx.embeds[0].url, headers=headers)
         print("Sent "+ctx.embeds[0].url)
     else:
         print(settings["bot"]+" bot not supported")
@@ -31,19 +35,25 @@ async def on_ready():
 
 @client.event
 async def on_message(ctx):
-    global keywords, channels
+    global keywords, channels, domains
     refresh()
     if not str(ctx.channel.id) in channels:
         return
     for keyword in keywords:
         keywordList = keyword.split(",")
-        good = True
+        goodKw = True
+        goodDom = False
         for kw in keywordList:
             if not kw.lower() in str(ctx.embeds[0].title).lower():
-                good = False
+                goodKw = False
                 break
-        if good:
+        for domain in domains:
+            if domain in str(ctx.embeds[0].url).lower():
+                goodDom = True
+                break
+        if goodKw and goodDom:
             go(ctx)
+            return
 
 
 
